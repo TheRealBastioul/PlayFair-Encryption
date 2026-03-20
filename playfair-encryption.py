e6996cf6daeba457b7f810664c930563590f34e4
BastThePM <152586621+TheRealBastioul@users.noreply.github.com>
2026-03-19 23:08:20 -0400

    Add Playfair cipher implementation
    Implement Playfair cipher encryption and decryption

---
+++ playfair-encryption.py
@@ -0,0 +1,110 @@
+import string
+
+def prepare_plaintext(text, filler):
+    text = text.lower().replace('j', 'i')
+    text = ''.join(c for c in text if c.isalpha())
+    
+    prepared = ''
+    i = 0
+    while i < len(text):
+        prepared += text[i]
+        if i + 1 < len(text) and text[i] == text[i + 1]:
+            prepared += filler
+        i += 1
+    
+    if len(prepared) % 2 != 0:
+        prepared += filler
+    
+    return prepared
+
+def playfair(keyword):
+    grid = [[], [], [], [], []]
+    gridindex = 0
+    alphabet = string.ascii_lowercase.replace('j', '')
+    seen = set()
+
+    for char in keyword.lower().replace('j', 'i'):
+        if char in seen:
+            continue
+        seen.add(char)
+        alphabet = alphabet.replace(char, '')
+        grid[gridindex].append(char)
+        if len(grid[gridindex]) % 5 == 0:
+            gridindex += 1
+
+    for char in alphabet:
+        grid[gridindex].append(char)
+        if len(grid[gridindex]) % 5 == 0:
+            gridindex += 1
+
+    return grid
+
+def lookup(grid):
+    lkp = {}
+    for r, row in enumerate(grid):
+        for c, char in enumerate(row):
+            lkp[char] = (r, c)
+    return lkp
+
+def playfair_pair(letter1, letter2, grid, lkp):
+    r1, c1 = lkp[letter1]
+    r2, c2 = lkp[letter2]
+
+    if r1 == r2:
+        return grid[r1][(c1 + 1) % 5], grid[r2][(c2 + 1) % 5]
+    elif c1 == c2:
+        return grid[(r1 + 1) % 5][c1], grid[(r2 + 1) % 5][c2]
+    else:
+        return grid[r1][c2], grid[r2][c1]
+
+def playfair_pair_decrypt(letter1, letter2, grid, lkp):
+    r1, c1 = lkp[letter1]
+    r2, c2 = lkp[letter2]
+
+    if r1 == r2:
+        return grid[r1][(c1 - 1) % 5], grid[r2][(c2 - 1) % 5]
+    elif c1 == c2:
+        return grid[(r1 - 1) % 5][c1], grid[(r2 - 1) % 5][c2]
+    else:
+        return grid[r1][c2], grid[r2][c1]
+
+def playfair_encrypt(plaintext, keyword, filler):
+    grid = playfair(keyword)
+    lkp = lookup(grid)
+    plaintext = prepare_plaintext(plaintext, filler)
+
+    pairs = []
+    for i in range(0, len(plaintext), 2):
+        pairs.append((plaintext[i], plaintext[i + 1]))
+
+    result = ''
+    for l1, l2 in pairs:
+        e1, e2 = playfair_pair(l1, l2, grid, lkp)
+        result += e1 + e2
+
+    return result
+
+def playfair_decrypt(ciphertext, keyword, filler):
+    grid = playfair(keyword)
+    lkp = lookup(grid)
+    ciphertext = ciphertext.lower().replace('j', 'i')
+    ciphertext = ''.join(c for c in ciphertext if c.isalpha())
+
+    pairs = []
+    for i in range(0, len(ciphertext), 2):
+        pairs.append((ciphertext[i], ciphertext[i + 1]))
+
+    result = ''
+    for l1, l2 in pairs:
+        d1, d2 = playfair_pair_decrypt(l1, l2, grid, lkp)
+        result += d1 + d2
+
+    return result
+
+encryption_item = input("Enter what you want to be encrypted: ")
+keyphrase = input("What is the keyphrase for the cipher? ")
+filler_item = input("Please enter the agreed filler in the cipher? ")
+encrypted_final = playfair_encrypt(encryption_item, keyphrase, filler_item)
+decrypted_final = playfair_decrypt(encrypted_final, keyphrase, filler_item)
+print(f'Encrypted: {encrypted_final}')
+print(f'Decrypted: {decrypted_final}')
